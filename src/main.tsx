a81382cee533a2e7f4cea54f8eb9419a2c5d60e1
Shay Shabtay <shay.shabshay@gmail.com>
2026-01-13 21:30:03 +0200

    Configure GitHub Pages SPA routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { ExpenseServiceProvider } from "./services/ExpenseServiceContext";
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <ExpenseServiceProvider>
         <App />
       </ExpenseServiceProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
