074c729f67ef1f0c9e8b5d5fce4f2bb456edca27
Shay Shabtay <shay.shabshay@gmail.com>
2026-01-13 20:55:14 +0200

    Configure GitHub Pages deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { ExpenseServiceProvider } from "./services/ExpenseServiceContext";
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ExpenseServiceProvider>
         <App />
       </ExpenseServiceProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
